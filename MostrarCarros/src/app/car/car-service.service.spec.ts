0490f3e4c21550aad782c02eb8bd53f7c4a2644c
julio-c-s <jc.cardenass12@uniandes.edu.co>
2023-04-30 11:57:52 -0500

    added the image

--- MostrarCarros/src/app/car/car-service.service.spec.ts
+++ MostrarCarros/src/app/car/car-service.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CarServiceService } from './car-service.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: CarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
       providers: [CarServiceService]
     });
   });
